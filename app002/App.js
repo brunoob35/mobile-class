bce43502b9d21a9c31ed809634de0f86bb1e7a89
brunoob35 <bruno_schmaiske_quoos@hotmail.com>
2026-03-05 15:49:33 -0300

    Transforma projetos em pastas normais dentro do monorepo

---
+++ app002/App.js
@@ -0,0 +1,16 @@
+//importar a biblioteca geral do React
+import React from 'react';
+//importar os componentes View e Text
+import { View, Text } from 'react-native';
+
+function App() {
+    return (
+            <View>
+                <Text>App002</Text>
+                <Text>Bruno S Quoos</Text>
+                <Text>19/02/2025</Text>
+
+            </View>
+            );
+}
+export default App;
